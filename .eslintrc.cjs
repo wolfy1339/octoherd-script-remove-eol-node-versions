88596dd94b2798a7547229c0ecb4eb803531e4c0
wolfy1339 <webmaster@wolfy1339.com>
2022-06-26 22:41:12 -0400

    chore(lint): add eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true
+  },
+  extends: [
+    '@hellomouse/wolfy1339'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    'no-unused-vars': 1,
+    'valid-jsdoc': 0
+  }
+};
